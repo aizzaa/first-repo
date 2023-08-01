c4e6fcd6ce8f20ff28bd638f1bb9b653745e9d55
Aiza <aizza.1897@gmail.com>
2023-08-01 19:28:28 +0600

    first commit

---
+++ git_commands.py
@@ -0,0 +1,16 @@
+# git - это система управления версиями кода
+
+# github - сайт, который дает хранилище для кода
+# также есть gitlab, bitbucket
+
+# репозиторий - папка с проектом с которой может работать git
+
+# ssh-keygen - создание ключа
+
+# git config --global user.name "your name"
+# git config --global user.email "your email"
+# указание своих данных
+
+# git init - первратить папку в репозиторий, создает скрытую папку .git
+
+
